8b27fb2e09d00e96c2970bcc239904d9d483660b
Jonathon Squire <jgsquire27@gmail.com>
2022-04-13 20:49:28 -0600

    began creating display cards function for cardset page

--- js/card-set.js
+++ js/card-set.js
@@ -1,1 +1,34 @@
-console.log(loadData())
+// function displayCardSets(data) {
+//   const cardSetOne = data.cards.slice(0,12);
+//   console.log(cardSetOne);
+
+
+//   const cardSetTwo = data.cards.slice(12,24);
+//   console.log(cardSetTwo);
+  
+
+//   const cardSetThree = data.cards.slice(24,36);
+//   console.log(cardSetThree);
+
+//   for (let i = 0; i < cardSetOne.length; i++) {
+//     createImgElement=(i)=>{
+//       let cardPackOne = document.getElementById("set-one").appendChild(document.createElement('img'));
+//       cardPackOne.className = 'cardPackClass';
+//       cardPackOne.src = cardSetOne[i].imgUrl;
+//       return cardPackOne; 
+//     }
+
+//   }
+
+
+  // const cardSetOneDiv = document.getElementById("card-set-one-parent");
+  // const cardSetImg = cardSetOne[0].imageUrl;
+  
+// }
+
+/*I need to somehow display the card images from the API but i dont
+see any images other than the logos in the API array
+
+I want to go look at the API website*/
+
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,56 @@
 //card pack api 
 const dataPromise = loadData();
-dataPromise.then(data => console.log(data.sets[0]));
+dataPromise.then(data => {
+    displayCardSets(data);
+    // console.log(data.cards);
+
+});
+
+//----------------------------------------------------//
+
+
+
+function displayCardSets(data) {
+    const cardSetOne = data.cards.slice(0,12);
+    console.log(cardSetOne);
+  
+  
+    const cardSetTwo = data.cards.slice(12,24);
+    // console.log(cardSetTwo);
+    const cardSetThree = data.cards.slice(24,36);
+    // console.log(cardSetThree);
+    let parentElement = document.getElementById('set-one');
+      
+    for (let i = 0; i < cardSetOne.length; i++) {
+      createImgElement=(i)=>{
+        let newImg = document.createElement('image');
+        newImg.src = cardSetOne[i].imgUrl;
+        parentElement.appendChild(newImg);
+
+
+        // parentElement.appendChild(document.createElement('img'));    
+        // let cardPackOne;
+        // cardPackOne.className = 'cardPackClass';
+        // cardPackOne.src = cardSetOne[i].imgUrl;
+        // return cardPackOne; 
+      }
+      return newImg;
+  
+    }
+
+}
+
+
+
+
+    //----------------------------------------------------------------//
+
+
+// dataPromise.then(data => console.log(data.sets[1]));
 
 
 async function loadData() {
-    return fetch('https://api.pokemontcg.io/v1/sets').then(response => response.json());
+    return fetch('https://api.pokemontcg.io/v1/cards').then(response => response.json());
 }
 
 //change buttons to display selected option and update local storage to selected options
@@ -14,7 +60,7 @@ localStorage.setItem('pack', 'pack1');
 
 document.getElementById('hidden-players').addEventListener('change', function(e) {
     let playerId = e.target.options[e.target.selectedIndex].getAttribute('id');
-    localStorage.setItem('players', playerId);
+    localStorage.setItem('players', playerId);  
 });
 
 document.getElementById('hidden-size').addEventListener('change', function(e) {
