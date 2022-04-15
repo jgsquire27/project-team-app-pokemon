446a1dd5257820495aaeed45fac321860e509fd6
Noah Coleman <noahcoleman@Noahs-MacBook-Pro-2.local>
2022-04-15 08:08:41 -0600

    cleaner script.js and moved cardset to cardset js

--- js/card-set.js
+++ js/card-set.js
@@ -1,34 +1,38 @@
-// function displayCardSets(data) {
-//   const cardSetOne = data.cards.slice(0,12);
-//   console.log(cardSetOne);
+//card pack api 
+let localApi = localStorage.getItem('localApi');
+async function loadData() {
+    if (localApi === '' || localApi === null) {
+        let renderApi = JSON.stringify(await fetch('https://api.pokemontcg.io/v1/cards').then(response => response.json()));
+        localStorage.setItem('localApi', renderApi);
+    }
+    else {
+        localApi = JSON.parse(localStorage.getItem('localApi'));
+    }
+    console.log(localApi);
+}
+loadData();
 
+function displayCardSets(data) {
+    const cardSetOne = data.cards.slice(0,12);  
+    const cardSetTwo = data.cards.slice(12,24);
+    const cardSetThree = data.cards.slice(24,36);
+    let parentElement = document.getElementById('set-one');
+    let parentElementTwo = document.getElementById('set-two');
+    let parentElementThree = document.getElementById('set-three');
 
-//   const cardSetTwo = data.cards.slice(12,24);
-//   console.log(cardSetTwo);
-  
-
-//   const cardSetThree = data.cards.slice(24,36);
-//   console.log(cardSetThree);
-
-//   for (let i = 0; i < cardSetOne.length; i++) {
-//     createImgElement=(i)=>{
-//       let cardPackOne = document.getElementById("set-one").appendChild(document.createElement('img'));
-//       cardPackOne.className = 'cardPackClass';
-//       cardPackOne.src = cardSetOne[i].imgUrl;
-//       return cardPackOne; 
-//     }
-
-//   }
-
-
-  // const cardSetOneDiv = document.getElementById("card-set-one-parent");
-  // const cardSetImg = cardSetOne[0].imageUrl;
-  
-// }
-
-/*I need to somehow display the card images from the API but i dont
-see any images other than the logos in the API array
-
-I want to go look at the API website*/
-
-
+    for (let i = 0; i < cardSetOne.length; i++) {
+        let newImg = document.createElement('img');
+        newImg.src = cardSetOne[i].imageUrl;
+        parentElement.appendChild(newImg);
+    }
+    for (let i = 0; i < cardSetTwo.length; i++) {
+        let newImg = document.createElement('img');
+        newImg.src = cardSetTwo[i].imageUrl;
+        parentElementTwo.appendChild(newImg);
+    }
+    for (let i = 0; i < cardSetThree.length; i++) {
+        let newImg = document.createElement('img');
+        newImg.src = cardSetThree[i].imageUrl;
+        parentElementThree.appendChild(newImg);
+    }
+}
--- js/script.js
+++ js/script.js
@@ -12,51 +12,6 @@ async function loadData() {
 }
 loadData();
 
-//----------------------------------------------------//
-
-
-
-function displayCardSets(data) {
-    //Creates first card set array//
-    const cardSetOne = data.cards.slice(0,12);  
-    //Creates second card set array//
-    const cardSetTwo = data.cards.slice(12,24);
-    //Creates third card set array//
-    const cardSetThree = data.cards.slice(24,36);
-    //links js to parent
-    let parentElement = document.getElementById('set-one');
-    for (let i = 0; i < cardSetOne.length; i++) {
-        //creates an image element
-        let newImg = document.createElement('img');
-        //assigns image url to elements source
-        newImg.src = cardSetOne[i].imageUrl;
-        //adds child image to parent div
-        parentElement.appendChild(newImg);
-    }
-
-    let parentElementTwo = document.getElementById('set-two');
-    for (let i = 0; i < cardSetTwo.length; i++) {
-        let newImg = document.createElement('img');
-        newImg.src = cardSetTwo[i].imageUrl;
-        parentElementTwo.appendChild(newImg);
-    }
-
-    let parentElementThree = document.getElementById('set-three');
-    for (let i = 0; i < cardSetThree.length; i++) {
-        let newImg = document.createElement('img');
-        newImg.src = cardSetThree[i].imageUrl;
-        parentElementThree.appendChild(newImg);
-    }
-}
-
-
-
-
-    //----------------------------------------------------------------//
-
-
-
-
 //change buttons to display selected option and update local storage to selected options
 let startButton = document.getElementById('start-button');
 document.getElementById('hidden-players').addEventListener('change', function(e) {
